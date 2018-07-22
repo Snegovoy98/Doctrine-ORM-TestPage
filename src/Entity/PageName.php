a4f778e2c9881cba5f7f64fdb82bb1a561f4f5f4
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-22 20:48:33 +0300

    Add PHPDoc for class PageName

--- src/Entity/PageName.php
+++ src/Entity/PageName.php
@@ -9,6 +9,9 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class PageName
 {
+    /**This class contains information about homePage
+     *
+     */
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
