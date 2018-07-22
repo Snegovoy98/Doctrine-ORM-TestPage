3a3c3d6444b5a995953008b01955c9f27823a51f
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-22 23:46:12 +0300

    Change name on Album

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -5,9 +5,9 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\PageNameRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\AlbumRepository")
  */
-class PageName
+class Album
 {
     /**This class contains information about homePage
      *
