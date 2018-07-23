76306061bbcbe0d4715b3e85e2f4f2a7732715e0
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-23 08:32:23 +0300

    Rename on AlbumController

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends AbstractController
+class AlbumController extends AbstractController
 {
     public function index()
     {
