ae0a6aacfb2efb22368c7c821c3ca22a425f4a9c
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-23 19:01:56 +0300

    Rename controller name

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
+use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AlbumController extends AbstractController
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\DBAL\DBALException
+     */
     public function index()
     {
+        /** @var AlbumRepository $repository */
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository(Album::class);
+        $text = $repository->getAllText();
+        var_dump($text);
+
         return $this->render('index/index.html.twig');
     }
 }
