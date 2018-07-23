1e432f19922453236e97ae6ec735ad1025ff47de
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-07-23 09:09:30 +0300

    Add PHPDoc in SQL request

--- src/Migrations/Version20180722175156.php
+++ src/Migrations/Version20180722175156.php
@@ -54,6 +54,7 @@ final class Version20180722175156 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE page_name');
+        $this->addSql(/** @lang text */
+            'DROP TABLE page_name');
     }
 }
